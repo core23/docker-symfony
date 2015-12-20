3e603817f202d10a8cd63e1900fbe414bf8092cd
core23 <mail@core23.de>
2015-12-20 18:07:56 +0100

    Updated nginx config

--- nginx/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/bin/sed -i "s@<APP_SERVER_NAME>@${APP_SERVER_NAME}@" /etc/nginx/sites-enabled/default
-/bin/sed -i "s@<PHP_HOST>@${PHP_1_PORT_9000_TCP_ADDR}@" /etc/nginx/sites-enabled/default
-/bin/sed -i "s@<PHP_PORT>@${PHP_1_PORT_9000_TCP_PORT}@" /etc/nginx/sites-enabled/default
-
-/usr/sbin/nginx
